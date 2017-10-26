4f9288bcfd20e54abcd69683b94c1bdd1778a2d5
Colin Froggatt <colin@froggatt.com>
2017-10-26 15:13:31 +0000

    extract redundant code and prp for prod

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,24 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article_from_id, only: [:show, :edit, :update, :destroy]
+  
   def index
     @articles = Article.all
     
   end
-  
   
   def new
     @article = Article.new
   end
   
   def edit 
-    @article = Article.find(params[:id])
   end
   
   def  create
     @article = Article.new(article_params)
     
     if @article.save
-      flash[:notice] = "article was successfuilly created"      
+      flash[:notice] = "article was successfully created"      
       redirect_to article_path(@article)
     else
       render 'new'  
@@ -28,30 +28,29 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice] = "article was successfuilly updated"      
+      flash[:notice] = "article was successfully updated"      
       redirect_to article_path(@article)
     else
       render 'edit'  
     end
-    
-    
-    
   end
     
   def show
-    @article = Article.find(params[:id])
   end
   
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
-    flash[:notice] = "article was successfuilly deleted"
+    flash[:notice] = "article was successfully deleted"
     redirect_to articles_path
   end
   
   private
+  
+    def set_article_from_id
+      @article = Article.find(params[:id])
+    end
+  
     def article_params
       params.require(:article).permit(:title, :description)
     end
