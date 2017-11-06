224040ca889a053d6f313a2a315622675b563ff4
Colin Froggatt <colin@froggatt.com>
2017-11-06 08:57:06 +0000

    styled errors and flash messages. refactored common code.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     
     if @article.save
-      flash[:notice] = "article was successfully created"      
+      flash[:success] = "article was successfully created"      
       redirect_to article_path(@article)
     else
       render 'new'  
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   
   def update
     if @article.update(article_params)
-      flash[:notice] = "article was successfully updated"      
+      flash[:success] = "article was successfully updated"      
       redirect_to article_path(@article)
     else
       render 'edit'  
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   
   def destroy
     @article.destroy
-    flash[:notice] = "article was successfully deleted"
+    flash[:danger] = "article was successfully deleted"
     redirect_to articles_path
   end
   
