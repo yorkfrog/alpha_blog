efd76e852d702cc88064f977d5c67e9cbb0d65d9
Colin Froggatt <colin@froggatt.com>
2017-11-28 14:02:31 +0000

    edit/update user partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,35 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"      
+      redirect_to articles_path
+    else
+      render 'edit'  
+    end
+  end
+    
+#  def show
+#  end
+  
+#  def destroy
+#    @article.destroy
+#    flash[:danger] = "article was successfully deleted"
+#    redirect_to articles_path
+#  end  
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
   
-  
-  
+ 
+
 end
   
