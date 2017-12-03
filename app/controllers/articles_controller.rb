2855b97aff3c1c4b93f328def70d369046572311
Colin Froggatt <colin@froggatt.com>
2017-12-03 12:19:30 +0000

    use current user on article crewte

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     # hardcode user for dev. until we get authentication working
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "article was successfully created"      
       redirect_to article_path(@article)
