35ef4f564b42712e7d666b814b3c4aeb182d4053
Colin Froggatt <colin@froggatt.com>
2017-12-03 13:28:33 +0000

    add admin user func.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index 
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -40,6 +41,14 @@ class UsersController < ApplicationController
     
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles have been deleted"
+    redirect_to users_path
+  end
+
+
   def set_user 
     @user = User.find(params[:id])
   end
@@ -56,11 +65,20 @@ class UsersController < ApplicationController
     end
     
     def require_same_user 
-      if current_user != @user
+      if current_user != @user && !current_user.admin?
         flash[:danger] = "You can only edit your own account."
         redirect_to root_path
       end
     end 
+
+    def require_admin
+      if logged_in? && !current_user.admin?
+        flash[:danger] = "Only admin's can perform this action."
+        redirect_to root_path
+      end
+    end
+    
+
 
 end
   
