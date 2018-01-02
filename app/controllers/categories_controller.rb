d2cde22538c9c739872d36267ea6f3d91f0c87d3
Colin Froggatt <colin@froggatt.com>
2018-01-02 16:48:00 +0000

    allow editing of categories by admins

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,14 +18,27 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
     if @category.save
-      flash[:success] = "Category was created successfully"#
+      flash[:success] = "Category was created successfully"
       redirect_to categories_path
     else
       render 'new'
     end
   end
   
+  def edit
+    @category = Category.find(params[:id])
+    
+  end
   
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "Category name was successfully updated"
+      redirect_to categories_path(@category)
+    else
+      render 'edit'
+    end
+  end
   
   private
     def category_params
