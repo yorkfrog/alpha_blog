6cba1cf9ac8634ffc4e7faddde7556946e4a93db
Colin Froggatt <colin@froggatt.com>
2018-01-02 15:56:39 +0000

    update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,9 +10,9 @@ class CategoriesController < ApplicationController
     @category = Category.new  
   end
   
-  
   def show
-  
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
    
   def create
